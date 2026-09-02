63f05653f7d2025af399b7c6520a27cabb898fa0
Anju <anjalianjali0169@gmail.com>
2026-09-02 22:19:11 +0530

    Initial Flask todo api

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+todos = []
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    todo = request.json
+    todos.append(todo)
+    return jsonify(todo), 201
+
+@app.route('/todos/<int:index>', methods=['DELETE'])
+def delete_todo(index):
+    todos.pop(index)
+    return '', 204
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
